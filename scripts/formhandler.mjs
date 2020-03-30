e3ca48b11559aad6e8dcefe9aa6f5cdff781fa55
ligand <->
2020-03-30 21:44:18 +0800

    implement live editing capability for FormHandler and CoffeeFormHandler

--- scripts/formhandler.mjs
+++ scripts/formhandler.mjs
@@ -2,47 +2,95 @@ import {populateAndDispatch, dispatchToControl, elementIfExists} from './utils.j
 
 
 class FormHandler {
+		// # Live editing
+		// supports live editing form data. start this by calling this.startEditHandler, passing as argument
+		// a Map() containing the values to populate the form with and edit. Provide a callback function to
+		// addOnEditHandler which will be called everytime an edit is carried out.
+		//
+		// The form will hide the 'Submit' and 'Reset' button and will instead display a 'Done' button for when the use is done editing. Shows those two buttons again when done editing.
 		constructor (selector) {
 				this.formElement = elementIfExists(selector);
-
-				if (!this.formElement instanceof HTMLFormElement){
-						throw new Error('Not an HTMLFormElement' + selector)
-				}
-
+				this.submitButton = this.formElement.querySelector('button[type="submit"]')
 				this.resetButton = this.formElement.querySelector('button[type="reset"]')
-				// if element exists, register event handler
-				if(this.resetButton) this.resetButton.addEventListener('click', e => this.resetForm(e))
+				this.doneButton = this.formElement.querySelector('button[type="button"]')
+				this.editToSubmitState() // hides "Done" button
+
+				this.resetButton.addEventListener('click', e => this.resetForm(e))
+				this.doneButton.addEventListener('click', e => this.doneEditing())
+
+				this.isLiveEditing = false;
 		}
 
 		addSubmitHandler (fn) {
 				this.formElement.addEventListener('submit', event => {
 						event.preventDefault()
 
-						const data = new Map(
-								// get the data entered into this.formElement
-								new FormData(this.formElement)
-						)
 						// pass the form data to the callback
-						fn(data)
+						fn(this.getFormData())
 
-						this.formElement.reset()
+						this.resetForm()
 						this.formElement.elements[0].focus()
-						// form controls reset. dispatch input event so those depending on controls value can update
-						Array.from(this.formElement.elements)
-								.forEach(control => dispatchToControl(control, new Event('input')))
 				})
 		}
 
 		resetForm (event) {
+				// reset form controls. dispatch input event so those depending on controls value can update
 				// manually resetting instead of using a reset button as need to trigger input
-				// event on each form control
+				// event on each form control.
 				this.formElement.reset()
 				Array.from(this.formElement.elements)
 						.forEach(control => dispatchToControl(control, new Event('input')))
 		}
 
 		populate (formdata) {
-				populateAndDispatch(formElement, testData, new Event('input'));
+				populateAndDispatch(this.formElement, formdata, new Event('input'));
+		}
+
+		getFormData () {
+				return new Map(
+						// get the data entered into this.formElement
+						new FormData(this.formElement)
+				)
+		}
+
+		startLiveEdit (formData, onEditHandler, doneEditHandler) {
+				this.isLiveEditing = true;
+				this.doneEditHandler = doneEditHandler
+
+				this.populate(formData)
+				this.submitToEditState()
+				this.formElement.addEventListener('input', event => {
+						if(!this.isLiveEditing) return
+						onEditHandler(this.getFormData())
+				})
+		}
+
+		doneEditing () {
+				this.isLiveEditing = false;
+
+				this.doneEditHandler()
+				this.resetForm()
+				this.editToSubmitState()
+		}
+
+		submitToEditState () {
+				this.hideElement(this.submitButton)
+				this.hideElement(this.resetButton)
+				this.unhideElement(this.doneButton)
+		}
+
+		editToSubmitState () {
+				this.unhideElement(this.submitButton)
+				this.unhideElement(this.resetButton)
+				this.hideElement(this.doneButton)
+		}
+
+		hideElement (element) {
+				element.style.display = 'none';
+		}
+
+		unhideElement (element) {
+				element.style.display = 'initial';
 		}
 }
 
@@ -51,6 +99,8 @@ class CoffeeFormHandler extends FormHandler {
 				super(selector)
 
 				this.lastOrderID = 0;
+
+				this.liveEditingOrderID = undefined;
 		}
 
 		nextOrderID () {
@@ -60,10 +110,28 @@ class CoffeeFormHandler extends FormHandler {
 		addSubmitHandler (fn) {
 				super.addSubmitHandler(
 						formData => {
-								// include a unique order ID
+								// include a unique order ID which isnt part of the form
 								formData.set('orderid', this.nextOrderID())
 								fn(formData)
 						}
+				)
+		}
+
+		startLiveEdit (formData, onEditHandler, doneEditHandler) {
+				// formData: form data to start with
+				// onEditHandler: function to be called with the form data on each edit, onEditHandler.
+				// doneEditHandler: function to be called when done editing, doneEditHandler
+				this.liveEditingOrderID = formData.get('orderid')
+				formData.delete('orderid')
+
+				super.startLiveEdit(
+						formData,
+						formData => {
+								// include the order id which isnt part of the form
+								formData.set('orderid', this.liveEditingOrderID)
+								onEditHandler(formData)
+						},
+						doneEditHandler
 				)
 		}
 }
